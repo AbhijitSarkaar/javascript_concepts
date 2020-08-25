785a2ac6151b48cab0d6abdd68abbb1c6ba8c9e7
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-08-25 10:32:21 +0530

    Wrapper objects

---
+++ wrapperObjects.js
@@ -0,0 +1,17 @@
+/*
+    Resources
+    1. https://javascriptrefined.io/the-wrapper-object-400311b29151
+    2. https://programmingwithmosh.com/javascript/javascript-wrapper-objects/
+    3. https://www.javascripttutorial.net/javascript-primitive-wrapper-types/
+    
+    Concepts
+    1. Wrapper objects are present for number, string, boolean
+    2. They are used under the hood when a property is called on the primitive type (number, string, boolean)
+    3. Wrapper objects are of global scope
+    4. Wrapper object's valueOf() property returns the primitive value which it wraps
+    5. Wrapper objects are instantiated by JS Engine only when a property is accessed on a primitive value. The instance 
+    is deleted immediately after using it once
+    6. Wrapper objects example: String(), Number(), Boolean()
+    7. JavaScript treats primitive values as objects when executing methods and properties using wrapper objects
+*/
+
