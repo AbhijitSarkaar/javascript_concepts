26392af3ec323b8d5fc2a5cacb317056009845ee
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-10-04 09:17:53 +0530

    promise chain with example

--- promise_callbacks.js
+++ promise_callbacks.js
@@ -25,7 +25,54 @@ current
 
     1. then() method takes up to two arguments: callback functions for the success and failure. each call back function takes corresponding
     fullfilled value or the rejected value
-    2. then() returns a promise
-    3. then() is called asynchronously (i.e it's executed after all the js sync statements are executed )
-    
+    2. then() returns a promise. 
+    When a value is simply returned from within a then handler, it will effectively return Promise.resolve(<value returned by whichever handler was called>).
+    3. promise chaining is achieved by returning promise from then() method. the subsequent then methods receive the promise value from previous then methods
+    the subsequent then() methods wait for the promise to get resolved or rejected
+
 */
+
+//Example of promise chain
+//Output if setTimeout is disabled in second then
+/*
+First
+Second
+1Foobar
+(after 1 second) 
+2Foobarbaz
+3Foobarbaz
+*/
+//Output if setTimeout is enabled in second then
+/*
+First
+Second
+1Foobar
+3Foobar
+(after 1 second) 
+2Foobarbaz 
+*/
+
+console.log('First');
+Promise.resolve('Foo')
+	.then((value) => {
+		return new Promise((resolve, reject) => {
+			console.log('1' + value + 'bar');
+			resolve(value + 'bar');
+		});
+	})
+	.then((value) => {
+		// setTimeout(() => {
+		// 	console.log('2' + value + 'baz');
+		// }, 1000);
+		// return value;
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				console.log('2' + value + 'baz');
+				resolve(value + 'baz');
+			}, 3000);
+		});
+	})
+	.then((value) => {
+		console.log('3' + value);
+	});
+console.log('Second');
