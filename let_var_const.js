2cc72c8a271ebed473f029fca7d469441b0cf421
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-10-06 10:17:29 +0530

    let var and const

---
+++ let_var_const.js
@@ -0,0 +1,19 @@
+/*
+    Resources
+    1. https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/
+
+    Concepts
+    1. var is global and function scoped. let/const are block scoped
+    2. var is hoisted in the top of their scope and initialized with undefined. 
+       let/const are also hoisted but it's not initialized
+    3. var can be re-declared and updated. let can not be re-declared but it can be updated. const can not be re-declared and updated
+    4. const variables must be initialized during declarations
+    5. var global scoped variables are properties of global object which is not the case of let and const
+*/
+
+/*
+    Questions
+    Q. What is the keyword used for a variable used without keyword
+    A. It becomes a property of window object
+*/
+
