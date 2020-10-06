5a6c80838a02bff42dda84d94688b22fc176c9cb
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-10-07 00:27:18 +0530

    object copying

--- objects.js
+++ objects.js
@@ -4,6 +4,8 @@ Object
 
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
     https://javascript.info/object
+    https://www.freecodecamp.org/news/copying-stuff-in-javascript-how-to-differentiate-between-deep-and-shallow-copies-b6d8c1ef09cd/
+
     1. {} to created objects are called object initializer
     2. Square brackets to be used for property access if it's not valid javascript identifier https://docstore.mik.ua/orelly/webprog/jscript/ch02_07.htm#:~:text=An%20identifier%20is%20simply%20a,Java%20and%20many%20other%20languages.
     3. object property keys may be either of string type, or of symbol type https://javascript.info/symbol
@@ -13,18 +15,19 @@ Object
     7. for..in loop to iterate over the keys
     8. in operator can be used for checking keys
     9. delete operator to delete a property
+    10. spread and Object.assign() does deep copy. For nested objects need to use them for all the nested objects inside a object
 
-methods, variables
-this
-winddow objects
+prototypes (__proto__)
+prototype inheritence
+window objects
 object references
 what happens when one object is assigned to another object, about shallow/deep copy of the properties
 chaining
 spread operator, object assign()
-prototypes (__proto__)
-prototype inheritence
 
 Current
+this
+
 
 */
 
@@ -38,7 +41,7 @@ function Car(maker, model, year) {
 
 let car1 = new Car('Tata', 'Nixon', '2014');
 
-console.log(car1.maker, car1.model, car1.year);
+// console.log(car1.maker, car1.model, car1.year);
 
 //const keyword
 
@@ -48,8 +51,22 @@ const user = {
 
 user.name = 'Pete'; //can change the properties of a const object
 
-user = {
-	name: 'Harry', //this throws an error, as it's an attempt to change the const object
+// user = {
+// 	name: 'Harry', //this throws an error, as it's an attempt to change the const object
+// };
+
+// console.log(user.name);
+
+//Nested objects copying
+
+let tata = {
+	car: {
+		model: 'Nixon',
+	},
 };
 
-console.log(user.name);
+let tata1 = { ...tata };
+
+tata1.car.model = 'Swift';
+
+console.log(tata.car.model);
