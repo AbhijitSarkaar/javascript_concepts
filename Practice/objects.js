d29fbd8dcff5ddc27845267b6c76052c7d518b20
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-11-25 20:56:50 +0530

    setting prototype using supertypes prototype object

--- Practice/objects.js
+++ Practice/objects.js
@@ -62,5 +62,57 @@ function Foo(x) {
 
 let foo = Foo(5);
 let foo1 = Foo(5);
-console.log(foo.bar());
-console.log(foo1.bar());
+// console.log(foo.bar());
+// console.log(foo1.bar());
+
+//6
+//Whenever prototype is changed. constructor property of a object is deleted and set to Object
+//instanceof: used to check if a object is a instance of a given constructor
+//hasOwnProperty: used to check if a object has a property
+
+function House(numBedrooms) {
+	this.numBedrooms = numBedrooms;
+}
+
+//constructor is House
+House.prototype = {
+	color: 'white',
+};
+let myHouse = new House(4);
+
+console.log(myHouse.constructor === House); //false as constructor is deleted when prototype is assigned a new object.
+//It can be true if
+/*
+House.prototype = {
+	constructor: House
+}; 
+*/
+//is used
+// console.log(myHouse instanceof House); //true
+// console.log(myHouse instanceof Object); //true
+// console.log(myHouse.hasOwnProperty('numBedrooms')); //true
+console.log(myHouse.constructor); //constructor is Object now
+
+//7
+//Animal class
+function Animal() {}
+Animal.prototype.eat = function () {
+	console.log('eats');
+};
+
+//Bird, a subtype of Animal
+function Bird() {}
+
+Bird.prototype = Object.create(Animal.prototype);
+
+//Now the constructor for Bird is Animal function
+Bird.prototype.constructor = Bird; //sets the constructor of inherited objects from Bird as Bird, otherwise it will point to Animal.prototype.constructor/Animal function
+//Now all the Bird type instances will have constructor as Bird
+Bird.prototype.fly = function () {
+	console.log('Flying');
+};
+
+let peacock = new Bird();
+console.log(peacock.constructor);
+console.log(peacock.eat()); //uses the eat() method of Animal.prototype
+peacock.fly();
