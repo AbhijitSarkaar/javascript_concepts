9308202b30635bf47e18d0931cf5a9ced4ea45b7
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-12-04 07:37:10 +0530

    some and every

---
+++ ArrayMethods/every.js
@@ -0,0 +1,6 @@
+//this method is used to check if every element in an array passes a condition
+//returns a boolean value
+
+let list = [1, 2, 4, 5, 6];
+
+console.log(list.every((num) => num > 0));
---
+++ ArrayMethods/some.js
@@ -0,0 +1,6 @@
+//this method is used to check if any element in an array passes a condition
+//returns a boolean value
+
+let list = [-1, -1, -2, 9];
+
+console.log(list.some((num) => num > 0));
