dccb4492d1bd07f0a8117d9916fd87a9d19f5b45
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-12-13 14:43:01 +0530

    exercise of settimeout and setinterval

--- Browser/apis.js
+++ Browser/apis.js
@@ -9,12 +9,39 @@ function foo() {
 	console.log('Hi');
 }
 
-setTimeout(foo, 0); //passing a reference to the function
+// setTimeout(foo, 0); //passing a reference to the function
 
 function print(...rest) {
 	console.log(rest.join(' '));
 }
 
-let timerId = setTimeout(print, 1000, 'Hi', 'Abhi'); //passing the arguments to print function
-console.log(timerId);
+let timerId = setTimeout(print, 1000, 'Hi', 'Mark'); //passing the arguments to print function
+// console.log(timerId);
 clearTimeout(timerId); //cancels the scheduled event
+
+//setInterval
+
+// setInterval(print, 1000, 'Hi', 'Mark'); //print Hi Mark every 1 second
+
+//Exercise
+
+//a function which prints 1 -> 10 every second
+
+//using setTimeout
+
+function print(num) {
+	if(num > 10) return
+	setTimeout(() => {
+		console.log(num)
+		print(num + 1);
+	}, 1000)
+}
+print(1);
+
+//using setInterval
+
+let i = 1;
+timerId = setInterval(() => {
+	if(i < 11) console.log(i), i++;
+	else clearInterval(timerId)
+}, 1000)
